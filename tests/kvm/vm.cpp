60f7532a15eaf856cfc42979228804dbb2819af3
Jason Phan <td.satch@gmail.com>
2020-08-13 22:36:18 -0500

    kvm: Add test for IRQ chip

--- tests/kvm/vm.cpp
+++ tests/kvm/vm.cpp
@@ -32,3 +32,24 @@ TEST_CASE("Invalid memory slot", "[api]") {
 
     REQUIRE_THROWS(vm.user_memory_region(mem_region));
 }
+
+TEST_CASE("IRQ Chip (x86)", "[api]") {
+    vmm::kvm::system kvm;
+    vmm::kvm::vm vm {kvm.vm()};
+
+    REQUIRE_NOTHROW(vm.check_extension(KVM_CAP_IRQCHIP));
+    REQUIRE_NOTHROW(vm.irqchip());
+
+    kvm_irqchip irqchip1 {
+        .chip_id = KVM_IRQCHIP_PIC_MASTER,
+        .chip = { .pic = {.irq_base = 10} }
+    };
+    kvm_irqchip irqchip2 {
+        .chip_id = KVM_IRQCHIP_PIC_MASTER
+    };
+
+    REQUIRE_NOTHROW(vm.set_irqchip(&irqchip1));
+    REQUIRE_NOTHROW(vm.get_irqchip(&irqchip2));
+
+    REQUIRE(irqchip1.chip.pic.irq_base == irqchip2.chip.pic.irq_base);
+}
