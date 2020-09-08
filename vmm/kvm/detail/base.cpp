38ab332063ce8672c78667d1d30fbaaf8c9ca5aa
Jason Phan <td.satch@gmail.com>
2020-09-07 20:02:05 -0500

    kvm: Remove old comments

--- vmm/kvm/detail/base.cpp
+++ vmm/kvm/detail/base.cpp
@@ -7,41 +7,4 @@
 
 namespace vmm::kvm::detail {
 
-//KvmIoctl::~KvmIoctl() noexcept {
-    //if (!closed_) {
-        //try {
-            //utility::close(fd_);
-        //}
-        //catch (std::system_error& e) {
-            //// TODO
-        //}
-    //}
-//}
-
-/**
- * Closes the ioctl class handle.
- *
- * Use this if you'd like to handle possible failures of `utility::close()`.
- *
- * Examples
- * ========
- * ```
- * #include <vmm/kvm.hpp>
- *
- * auto kvm = vmm::kvm::system{};
- * auto vm = kvm.vm();
- * auto device = vm.device();
- *
- * try {
- *     device.close();
- * }
- * catch (std::system_error&)
- *     throw;
- * ```
- */
-//auto KvmIoctl::close() -> void {
-    //utility::close(fd_);
-    //closed_ = true;
-//}
-
 }  // namespace vmm::kvm::detail
