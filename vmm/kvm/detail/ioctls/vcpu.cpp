a9b6bd658a5627467a91093164e3a3922006e9a9
Jason Phan <jason@ket.so>
2020-10-25 18:46:05 -0500

    kvm: Implement the "basic" vCPU ioctls
    * KVM_GET_MP_STATE
    * KVM_SET_MP_STATE
    * KVM_GET_SREGS
    * SET_SREGS
    * KVM_GET_VCPU_EVENTS
    * KVM_SET_VCPU_EVENTS
    * KVM_GET_FPU
    * KVM_SET_FPU
    * KVM_GET_LAPIC
    * KVM_SET_LAPIC
    * KVM_GET_XSAVE
    * KVM_SET_XSAVE
    * KVM_GET_XCRS
    * KVM_SET_XCRS
    * KVM_GET_DEBUGREGS
    * KVM_SET_DEBUG_REGS
    * KVM_ARM_VCPU_INIT
    * KVM_GET_REGS
    * KVM_SET_REGS

--- vmm/kvm/detail/ioctls/vcpu.cpp
+++ vmm/kvm/detail/ioctls/vcpu.cpp
@@ -6,4 +6,114 @@
 
 namespace vmm::kvm::detail {
 
+#if defined(__i386__) || defined(__x86_64__)  || \
+    defined(__arm__)  || defined(__aarch64__) || \
+    defined(__s390__)
+        auto vcpu::mp_state() const -> kvm_mp_state {
+            auto mp_state = kvm_mp_state{};
+            m_fd.ioctl(KVM_GET_MP_STATE, &mp_state);
+            return mp_state;
+        }
+
+        auto vcpu::set_mp_state(const kvm_mp_state& mp_state) const -> void {
+            m_fd.ioctl(KVM_SET_MP_STATE, mp_state);
+        }
+#endif
+
+#if defined(__i386__) || defined(__x86_64__) || \
+    defined(__ppc__)  || defined(__ppc64__)
+        auto vcpu::sregs() const -> kvm_sregs {
+            auto sregs = kvm_sregs{};
+            m_fd.ioctl(KVM_GET_SREGS, &sregs);
+            return sregs;
+        }
+
+        auto vcpu::set_sregs(const kvm_sregs& sregs) const -> void {
+            m_fd.ioctl(KVM_SET_SREGS, sregs);
+        }
+#endif
+
+#if defined(__i386__) || defined(__x86_64__)  || \
+    defined(__arm__)  || defined(__aarch64__)
+        auto vcpu::vcpu_events() const -> kvm_vcpu_events {
+            auto vcpu_events = kvm_vcpu_events{};
+            m_fd.ioctl(KVM_GET_VCPU_EVENTS, &vcpu_events);
+            return vcpu_events;
+        }
+
+        auto vcpu::set_vcpu_events(const kvm_vcpu_events& vcpu_events) const -> void {
+            m_fd.ioctl(KVM_SET_VCPU_EVENTS, vcpu_events);
+        }
+#endif
+
+#if defined(__i386__) || defined(__x86_64__)
+        auto vcpu::fpu() const -> kvm_fpu {
+            auto fpu = kvm_fpu{};
+            m_fd.ioctl(KVM_GET_FPU, &fpu);
+            return fpu;
+        }
+
+        auto vcpu::set_fpu(const kvm_fpu& fpu) const -> void {
+            m_fd.ioctl(KVM_SET_FPU, fpu);
+        }
+
+        auto vcpu::lapic() const -> kvm_lapic_state {
+            auto lapic = kvm_lapic_state{};
+            m_fd.ioctl(KVM_GET_LAPIC, &lapic);
+            return lapic;
+        }
+
+        auto vcpu::set_lapic(const kvm_lapic_state& lapic) const -> void {
+            m_fd.ioctl(KVM_SET_LAPIC, lapic);
+        }
+
+        auto vcpu::xsave() const -> kvm_xsave {
+            auto xsave = kvm_xsave{};
+            m_fd.ioctl(KVM_GET_XSAVE, &xsave);
+            return xsave;
+        }
+
+        auto vcpu::set_xsave(const kvm_xsave& xsave) const -> void {
+            m_fd.ioctl(KVM_SET_XSAVE, xsave);
+        }
+
+        auto vcpu::xcrs() const -> kvm_xcrs {
+            auto xcrs = kvm_xcrs{};
+            m_fd.ioctl(KVM_GET_XCRS, &xcrs);
+            return xcrs;
+        }
+
+        auto vcpu::set_xcrs(const kvm_xcrs& xcrs) const -> void {
+            m_fd.ioctl(KVM_SET_XCRS, xcrs);
+        }
+
+        auto vcpu::debug_regs() const -> kvm_debugregs {
+            auto debug_regs = kvm_debugregs{};
+            m_fd.ioctl(KVM_GET_DEBUGREGS, &debug_regs);
+            return debug_regs;
+        }
+
+        auto vcpu::set_debug_regs(const kvm_debugregs& debug_regs) const -> void {
+            m_fd.ioctl(KVM_SET_DEBUGREGS, debug_regs);
+        }
+#endif
+
+#if defined(__arm__) || defined(__aarch64__)
+        auto vcpu::init(const kvm_vcpu_init& init) const -> void {
+            m_fd.ioctl(KVM_ARM_VCPU_INIT, init);
+        }
+#endif
+
+#if !defined(__arm__) && !defined(__aarch64__)
+        auto vcpu::regs() const -> kvm_regs {
+            auto regs = kvm_regs{};
+            m_fd.ioctl(KVM_GET_REGS, &regs);
+            return regs;
+        }
+
+        auto vcpu::set_regs(const kvm_regs& regs) const -> void {
+            m_fd.ioctl(KVM_SET_REGS, regs);
+        }
+#endif
+
 }  // namespace vmm::kvm::detail
--- vmm/kvm/detail/ioctls/vcpu.hpp
+++ vmm/kvm/detail/ioctls/vcpu.hpp
@@ -4,6 +4,10 @@
 
 #pragma once
 
+#include <cstddef> // size_t
+#include <cstdint> // uint32_t, uint64_t
+#include <linux/kvm.h> // kvm_*, KVM_*
+
 #include "vmm/kvm/detail/ioctls/vm.hpp"
 #include "vmm/kvm/detail/types/file_descriptor.hpp"
 
@@ -17,6 +21,47 @@ class vcpu
         KvmFd m_fd;
 
         explicit vcpu(int fd) noexcept : m_fd{fd} {}
+
+#if defined(__i386__) || defined(__x86_64__)  || \
+    defined(__arm__)  || defined(__aarch64__) || \
+    defined(__s390__)
+        auto mp_state() const -> kvm_mp_state;
+        auto set_mp_state(const kvm_mp_state&) const -> void;
+#endif
+
+#if defined(__i386__) || defined(__x86_64__) || \
+    defined(__ppc__)  || defined(__ppc64__)
+        auto sregs() const -> kvm_sregs;
+        auto set_sregs(const kvm_sregs&) const -> void;
+#endif
+
+#if defined(__i386__) || defined(__x86_64__)  || \
+    defined(__arm__)  || defined(__aarch64__)
+        auto vcpu_events() const -> kvm_vcpu_events;
+        auto set_vcpu_events(const kvm_vcpu_events&) const -> void;
+#endif
+
+#if defined(__i386__) || defined(__x86_64__)
+        auto fpu() const -> kvm_fpu;
+        auto set_fpu(const kvm_fpu&) const -> void;
+        auto lapic() const -> kvm_lapic_state;
+        auto set_lapic(const kvm_lapic_state&) const -> void;
+        auto xsave() const -> kvm_xsave;
+        auto set_xsave(const kvm_xsave&) const -> void;
+        auto xcrs() const -> kvm_xcrs;
+        auto set_xcrs(const kvm_xcrs&) const -> void;
+        auto debug_regs() const -> kvm_debugregs;
+        auto set_debug_regs(const kvm_debugregs&) const -> void;
+#endif
+
+#if defined(__arm__) || defined(__aarch64__)
+        auto init(const kvm_vcpu_init&) const -> void;
+#endif
+
+#if !defined(__arm__) && !defined(__aarch64__)
+        auto regs() const -> kvm_regs;
+        auto set_regs(const kvm_regs&) const -> void;
+#endif
 };
 
 }  // namespace vmm::kvm::detail
