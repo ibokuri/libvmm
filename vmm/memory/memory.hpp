d9f9f5c2ed4eab8eb34cdb4494f4a66a6faea731
Jason Phan <jason@ket.so>
2020-12-20 12:01:18 -0600

    memory: Remove GuestAddressValue in public header

--- vmm/memory/memory.hpp
+++ vmm/memory/memory.hpp
@@ -15,7 +15,6 @@ using AddressValue = vmm::memory::detail::AddressValue<T>;
 template<typename T, typename S>
 using Address = vmm::memory::detail::Address<T, S>;
 
-using GuestAddressValue = vmm::memory::detail::GuestAddressValue;
 using GuestAddress = vmm::memory::detail::GuestAddress;
 
 }  // vmm::memory
